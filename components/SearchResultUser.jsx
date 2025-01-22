23de0966ea681edd8b4c146e746dc1043118c198
James Michael <jomichael1994@gmail.com>
2025-01-22 22:36:12 +0000

    add SearchResultUser component and show the results

---
+++ components/SearchResultUser.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Link from 'next/link';
+
+const SearchResultUser = ({ user }) => {
+	console.log('user', user);
+	const { login, avatar_url, html_url } = user;
+
+	return (
+		<div className="flex flex-col h-44 gap-2.5 p-[1rem] justify-center items-center border-2 border-gray-200 rounded-lg hover:bg-gradient-to-br from-transparent hover:to-red-100 hover:cursor-pointer">
+			<img
+				src={avatar_url}
+				alt=""
+				className="rounded-full w-20 aspect-square"
+			/>
+			<span className="font-black">{login}</span>
+		</div>
+	);
+};
+
+export default SearchResultUser;

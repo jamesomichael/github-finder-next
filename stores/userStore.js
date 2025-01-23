793932600e22f3d2c933fc357dc000bfb4744166
James Michael <jomichael1994@gmail.com>
2025-01-23 16:53:57 +0000

    initialise user as an empty object in store

--- stores/userStore.js
+++ stores/userStore.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const useUserStore = create((set) => ({
 	isLoading: false,
-	user: null,
+	user: {},
 
 	setActiveUser: async (username) => {
 		set({ isLoading: true });

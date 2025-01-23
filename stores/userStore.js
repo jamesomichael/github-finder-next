e8a9350c8182087529eb88597438c3727b698e28
James Michael <jomichael1994@gmail.com>
2025-01-23 21:53:48 +0000

    update repository fetch to retrieve 30 items, sort by updated date

--- stores/userStore.js
+++ stores/userStore.js
@@ -13,7 +13,7 @@ const useUserStore = create((set) => ({
 		);
 		const { data: userData } = userResponse;
 		const reposResponse = await axios.get(
-			`https://api.github.com/users/${username}/repos?per_page=12&sort=created:asc`
+			`https://api.github.com/users/${username}/repos?per_page=30&sort=updated:asc`
 		);
 		const { data: reposData } = reposResponse;
 		set({ isLoading: false, user: userData, repos: reposData });

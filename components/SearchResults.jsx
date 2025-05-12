a77f46b034a9824ed82865132bc5138f205db1a7
James Michael <jomichael1994@gmail.com>
2025-05-12 20:09:24 +0100

    responsive changes, update user card backgrounds

--- components/SearchResults.jsx
+++ components/SearchResults.jsx
@@ -11,12 +11,11 @@ const SearchResults = () => {
 	return isLoading ? (
 		<Loader />
 	) : (
-		<div className="px-2 sm:px-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 sm:gap-4 w-full">
+		<div className="px-2 sm:px-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-5 gap-2 sm:gap-4 w-full">
 			{users.map((user) => (
 				<UserCard key={user.id} user={user} />
 			))}
 		</div>
-		// <pre>{JSON.stringify(users, null, 4)}</pre>
 	);
 };
 
--- components/UserCard.jsx
+++ components/UserCard.jsx
@@ -8,14 +8,16 @@ const UserCard = ({ user }) => {
 	return (
 		<Link
 			href={`/user/${login}`}
-			className="flex flex-col h-44 gap-2.5 p-[1rem] justify-center items-center border-2 border-gray-200 dark:bg-neutral-800 dark:text-white rounded-lg hover:bg-gradient-to-br from-transparent hover:to-red-100 hover:cursor-pointer"
+			className="w-full grid grid-cols-[auto_1fr] gap-4 sm:flex sm:flex-col sm:h-44 sm:gap-2.5 p-[1rem] justify-center items-center outline outline-1 hover:outline-2 outline-neutral-400 hover:outline-red-800 dark:outline-neutral-500 dark:bg-neutral-800 dark:text-white rounded-lg dark:hover:outline-neutral-300 hover:cursor-pointer"
 		>
 			<img
 				src={avatar_url}
 				alt=""
-				className="rounded-full w-20 aspect-square"
+				className="rounded-full h-11 sm:h-auto sm:w-20 aspect-square"
 			/>
-			<span className="font-black">{login}</span>
+			<div className="w-full sm:flex sm:justify-center truncate">
+				<span className="font-black truncate">{login}</span>
+			</div>
 		</Link>
 	);
 };
